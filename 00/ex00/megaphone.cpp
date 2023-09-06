dff367088b475cafbeb6b7b7ee2f414123d55f2a
tomatte <daniel.brandao2000@gmail.com>
2023-09-06 08:02:01 -0300

    is it better???

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,22 +1,36 @@
 #include <string>
 #include <iostream>
 
-void    scream(const char *s) {
-	std::string str = s;
+void    scream(std::string & str) {
+
 	for (int i = 0; i < (int) str.length(); i++) {
 		str[i] = std::toupper(str[i]);
 	}
 	std::cout << str;
 }
 
+void	feed_back_noise(void) {
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+}
+
 void    print_args(int argc, const char **argv) {
+	std::string	str;
+	bool		is_empty;
+
+	is_empty = true;
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		feed_back_noise();
 	for (int i = 1; i < argc; i++) {
-		if (i > 1)
+		str = argv[i];
+		if (i > 1 && is_empty == false && str != "")
 			std::cout << " ";
-		scream(argv[i]);
+		if (str != "")
+			is_empty = false;
+		if (str != "")
+			scream(str);
 	}
+	if (is_empty == true && argc > 1)
+		feed_back_noise();
 	std::cout << std::endl;
 }
 
