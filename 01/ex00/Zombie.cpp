611557b06d2183e43e841b68cab53554be5c72f1
tomatte <daniel.brandao2000@gmail.com>
2023-09-07 09:16:57 -0300

    safer to be evaluated and added header

--- 01/ex00/Zombie.cpp
+++ 01/ex00/Zombie.cpp
@@ -1,17 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrandao <dbrandao@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 09:12:07 by dbrandao          #+#    #+#             */
+/*   Updated: 2023/09/07 09:15:41 by dbrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(void) {
-}
+Zombie::Zombie(void) {}
 
-Zombie::Zombie(std::string name) : _name(name) {
-}
+Zombie::Zombie(std::string name) : name(name) {}
 
 Zombie::~Zombie(void) {
-	std::cout << this->_name << " got destroyed!!!\n";
+	std::cout << this->name << " got destroyed!!!\n";
 }
 
 void	Zombie::announce(void) {
-	std::cout << this->_name << ": BraiiiiiiinnnzzzZ...\n";
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ...\n";
 }
-
--- 01/ex00/Zombie.hpp
+++ 01/ex00/Zombie.hpp
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrandao <dbrandao@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 09:12:14 by dbrandao          #+#    #+#             */
+/*   Updated: 2023/09/07 09:12:14 by dbrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_H
 # define ZOMBIE_H
 
 #include <string>
 
 class Zombie {
-	std::string	_name;
+	std::string	name;
 
 	public:
 		Zombie(void);
