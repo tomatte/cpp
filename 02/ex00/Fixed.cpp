8dc52ac1b2b82d31fedf1c367a9af6ef775d9704
tomatte <daniel.brandao2000@gmail.com>
2023-09-26 07:20:24 -0300

    change behavior so output became equal so subject

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -1,21 +1,22 @@
 #include "Fixed.hpp"
 #include <iostream>
 
+int const Fixed::fractionalBits = 8;
+
 Fixed::Fixed(void) : fixed(0) {
 	std::cout << "Default constructor called\n";
 }
 
-Fixed::Fixed(Fixed const & rhs) : fixed(rhs.fixed) {
+Fixed::Fixed(Fixed const & rhs) {
 	std::cout << "Copy constructor called\n";
+	*this = rhs;
 }
 
 Fixed &	Fixed::operator=(Fixed const & rhs) {
-	this->fixed = rhs.fixed;
 	std::cout << "Copy assignment operator called\n";
+	this->fixed = rhs.getRawBits();
 	return (*this);
 }
-
-int const Fixed::fractionalBits = 8;
 
 int		Fixed::getRawBits(void) const {
 	std::cout << "getRawBits member function called\n";
