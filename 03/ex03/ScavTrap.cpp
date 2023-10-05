3675e1e0b6a07bfac40a98b74ae62b4c511d3ec1
tomatte <daniel.brandao2000@gmail.com>
2023-10-05 07:07:16 -0300

    eba eba

--- 03/ex03/ScavTrap.cpp
+++ 03/ex03/ScavTrap.cpp
@@ -54,9 +54,9 @@ void	ScavTrap::attack(std::string const & target)
 		std::cout << "Target " << target << " doesn't exist.\n";
 		return ;
 	}
-	std::cout << "ScavTrap " << this->get_name() << " attacks " << clap_target->get_name() \
-		<< ", causing " << this->get_attack_damage() << " points of damage!\n";
-	clap_target->takeDamage(this->get_attack_damage());
+	std::cout << "ScavTrap " << this->name << " attacks " << clap_target->get_name() \
+		<< ", causing " << this->attack_damage << " points of damage!\n";
+	clap_target->takeDamage(this->attack_damage);
 	this->energy_points--;
 }
 
@@ -64,15 +64,15 @@ void	ScavTrap::guardGate(void)
 {
 	if (this->is_dead())
 	{
-		std::cout << "ScavTrap " << this->get_name() << " is dead!\n";
+		std::cout << "ScavTrap " << this->name << " is dead!\n";
 		return ;
 	}
 	if (this->in_gate_keeper_mode == true)
 	{
-		std::cout << "ScavTrap " << this->get_name() << " is already in Gate keeper mode!\n";
+		std::cout << "ScavTrap " << this->name << " is already in Gate keeper mode!\n";
 		return ;
 	}
-	std::cout << "ScavTrap " << this->get_name() << " is now in Gate keeper mode.\n";
+	std::cout << "ScavTrap " << this->name << " is now in Gate keeper mode.\n";
 	this->in_gate_keeper_mode = true;
 	this->attack_damage /= 2;
 	this->energy_points = 100;
