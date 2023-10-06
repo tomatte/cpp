708797925ae3e9c542429c470ce7ac86ca621c22
tomatte <daniel.brandao2000@gmail.com>
2023-10-06 14:42:59 -0300

    implement AMateria

---
+++ 04/ex03/AMateria.cpp
@@ -0,0 +1,28 @@
+#include "AMateria.hpp"
+#include <iostream>
+
+AMateria::AMateria(void)
+{
+}
+
+AMateria::AMateria(std::string const & type) : type(type)
+{
+}
+
+AMateria::AMateria(AMateria const & rhs) : type(rhs.type)
+{
+}
+
+AMateria::~AMateria(void)
+{
+}
+
+std::string const & AMateria::getType() const
+{
+	return (this->type);
+}
+
+void AMateria::use(ICharacter& target)
+{
+	std::cout << "materia" << std::endl;
+}
--- 04/ex03/AMateria.hpp
+++ 04/ex03/AMateria.hpp
@@ -6,8 +6,15 @@
 class AMateria
 {
 	protected:
+		std::string const type;
+
 	public:
+		AMateria(void);
 		AMateria(std::string const & type);
+		AMateria(AMateria const & rhs);
+		~AMateria(void);
+		
+		AMateria & operator=(AMateria const & rhs);
 		std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
