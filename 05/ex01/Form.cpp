51859780103f24b9fac206e5624a26047eaf2ede
tomatte <daniel.brandao2000@gmail.com>
2023-10-11 12:51:00 -0300

    check error for form and throw it

--- 05/ex01/Form.cpp
+++ 05/ex01/Form.cpp
@@ -3,11 +3,19 @@
 Form::Form(void) : _name("Default"), _signed(false),
 _sign_grade(1), _execution_grade(1)
 {
+	this->error_check();
+}
+
+Form::Form(std::string & name, int sign_grade, int execution_grade) :
+_name(name), _signed(false), _sign_grade(sign_grade), _execution_grade(execution_grade)
+{
+	this->error_check();
 }
 
 Form::Form(Form const & rhs) : _name(rhs._name), _signed(rhs._signed),
 _sign_grade(rhs._sign_grade), _execution_grade(rhs._execution_grade)
 {
+	this->error_check();
 }
 
 Form::~Form(void)
@@ -19,4 +27,22 @@ Form & Form::operator=(Form const & rhs)
 	this->_name = rhs._name;
 	this->_signed = rhs._signed;
 	return (*this);
+}
+
+const char	*Form::GradeTooHighException::what(void) const throw()
+{
+	return "Grade is too high.";
+}
+
+const char	*Form::GradeTooLowException::what(void) const throw()
+{
+	return "Grade is too low.";
+}
+
+void	Form::error_check(void) const
+{
+	if (this->_execution_grade < 1 || this->_sign_grade < 1)
+		throw Form::GradeTooHighException();
+	if (this->_execution_grade > 150 || this->_sign_grade > 150)
+		throw Form::GradeTooLowException();
 }
--- 05/ex01/Form.hpp
+++ 05/ex01/Form.hpp
@@ -1,6 +1,7 @@
 #ifndef FORM_H
 # define FORM_H
 # include <string>
+# include <stdexcept>
 
 class Form
 {
@@ -9,11 +10,27 @@ class Form
 		bool		_signed;
 		int const	_sign_grade;
 		int const	_execution_grade;
+
+		void	error_check(void) const;
 	
 	public:
 		Form(void);
+		Form(std::string & name, int sign_grade, int execution_grade);
 		Form(Form const & rhs);
 		~Form(void);
+
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char	*what(void) const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char	*what(void) const throw();
+		};
 
 		Form & operator=(Form const & rhs);
 };
