a6b79aaa0dcab1b15b9d64956a726cdd7d1c2638
tomatte <daniel.brandao2000@gmail.com>
2023-10-18 10:04:22 -0300

    add some template functions

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
-
-template <typename T>
-T max(T & a, T & b)
-{
-	return (a >= b ? a : b);
-}
+#include "templates.hpp"
 
 int	main(void)
 {
-	double a = 5;
-	double	b = 8;
 
-	std::cout << "Max: " << max<double>(a, b);
+	std::cout << "---------------- Subject Test -----------------" << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "-------------------------------------------------" << std::endl;
+
+
+	std::string p = "pizza";
+	std::string	h = "hamburger";
+
+	swap(p, h);
+	std::cout << "p: " << p << std::endl;
+	std::cout << "h: " << h << std::endl;
+
+	const int x = 20;
+	const int y = 20;
+	std::cout << "x pointer:   " << &x << std::endl;
+	std::cout << "y pointer:   " << &y << std::endl;
+	std::cout << "min pointer: " << &(min(x, y)) << std::endl;
+	std::cout << "max pointer: " << &(max(x, y)) << std::endl;
+
+	const double w = 41.9;
+	const double z = 42;
+	std::cout << "\n\nw:   " << w << std::endl;
+	std::cout << "z:   " << z << std::endl;
+	std::cout << "min: " << (min(w, z)) << std::endl;
+	std::cout << "max: " << (max<double>(w, z)) << std::endl;
 	return (0);
 }
---
+++ 07/ex00/templates.hpp
@@ -0,0 +1,25 @@
+#ifndef TEMPLATES_H
+# define TEMPLATES_H
+
+template <typename T>
+void	swap(T & a, T & b)
+{
+	T c(a);
+
+	a = b;
+	b = c;
+}
+
+template <typename T>
+T const & min(T const & a, T const & b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T const & max(T const & a, T const & b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
