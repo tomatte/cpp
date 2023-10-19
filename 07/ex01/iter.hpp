fc3d559ef3eedb903ccd6ab00c7cd35a76e0add8
tomatte <daniel.brandao2000@gmail.com>
2023-10-19 18:23:31 -0300

    fix iter

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -1,8 +1,17 @@
 #ifndef ITER_H
 # define ITER_H
 
-template<typename T, typename U, typename F>
-void	iter(T *addr, U len, F func)
+template<typename T, typename U>
+void	iter(T *addr, U len, void (*func)(T &))
+{
+	for (U i = 0; i < len; i++)
+	{
+		func(addr[i]);
+	}
+}
+
+template<typename T, typename U>
+void	iter(T *addr, U len, void (*func)(T const &))
 {
 	for (U i = 0; i < len; i++)
 	{
