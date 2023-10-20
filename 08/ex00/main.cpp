0c6735517a8159075a2862f15e730a440e6884c2
tomatte <daniel.brandao2000@gmail.com>
2023-10-20 13:20:36 -0300

    tests

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,22 +6,29 @@
 
 int	main(void)
 {
+
+	ColorOut::blue << "-------- Using vector --------" << std::endl;
 	std::vector<int>	numbers;
 	numbers.push_back(19);
 	numbers.push_back(15);
 	numbers.push_back(10);
-
-	std::list<int>	names;
-	names.push_back(03);
-	names.push_back(01);
-	names.push_back(06);
 
 	const int	value1 = 10;
 	if (easyfind(numbers, value1) == numbers.end())
 		ColorOut::red << "Value " << value1 << " not found." << std::endl;
 	else
 		ColorOut::green << "Value " << value1 << " was found!!!" << std::endl;
-		
-	easyfind(names, 02);
+	
+	ColorOut::blue << std::endl << "-------- Using list --------" << std::endl;
+	std::list<int>	soldiers;
+	soldiers.push_back(03);
+	soldiers.push_back(01);
+	soldiers.push_back(06);
+
+	const int	value2 = 02;
+	if (easyfind(soldiers, value2) == soldiers.end())
+		ColorOut::red << "Value " << value2 << " not found." << std::endl;
+	else
+		ColorOut::green << "Value " << value2 << " was found!!!" << std::endl;
 	return (0);
 }
