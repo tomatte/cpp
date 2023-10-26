3e7633e7e606daf9e9031aa0077c47fd1468df4c
tomatte <daniel.brandao2000@gmail.com>
2023-10-26 16:39:06 -0300

    verify file opening

--- 09/ex00/BitcoinExchange.cpp
+++ 09/ex00/BitcoinExchange.cpp
@@ -12,11 +12,15 @@ bool	BitcoinExchange::is_valid_date(const char *str)
 
 void	BitcoinExchange::extract_data(std::string data_file)
 {
-	std::ifstream	file;
+	std::ifstream	file(data_file.c_str());
 	char			key[256];
 	char			value[256];
 
-	file.open(data_file.c_str());
+	if (file.fail())
+	{
+		std::cerr << "Error opening '" << data_file << "'" << std::endl;
+		return ;
+	}
 	file.getline(key, 256);
 	while (file.good() && file.eof() == false)
 	{
@@ -25,12 +29,11 @@ void	BitcoinExchange::extract_data(std::string data_file)
 		this->btc_data[key] = std::atof(value);
 	}
 	file.close();
-	std::cout << "first: " << this->btc_data.at("2017-09-27") << std::endl;
 }
 
 BitcoinExchange::BitcoinExchange(void)
 {
-	BitcoinExchange::extract_data("data.csv");
+	extract_data("data.csv");
 }
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const & rhs)
